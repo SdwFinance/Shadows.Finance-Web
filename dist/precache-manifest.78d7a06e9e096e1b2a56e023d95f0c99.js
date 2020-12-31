12a20ae89983413a5d15c0a7b68024a48247c375
Iwan <langhua292@163.com>
2020-12-31 14:10:35 +0800

    Upload dist foder

---
+++ dist/precache-manifest.78d7a06e9e096e1b2a56e023d95f0c99.js
@@ -0,0 +1,318 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c0b4c0b883b4c911cec2f7fcfaef95",
+    "url": "/Connect@2x.png"
+  },
+  {
+    "revision": "f3c3908dd1cb051c594bf9536e989223",
+    "url": "/checkbox-checked@2x.png"
+  },
+  {
+    "revision": "4bb4bac75c78b75d0b77f7090d2fd18a",
+    "url": "/checkbox@2x.png"
+  },
+  {
+    "revision": "25b5872b96809b7eb1630f904fb2a6f4",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "642de0bb60c29b63578454bbdaa75919",
+    "url": "/favicon-24x24.svg"
+  },
+  {
+    "revision": "06d03b59ea420dfde2cbc435f2f6840e",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "5d06b278cc1de637d76026991dd32f50",
+    "url": "/favicon-32x32.svg"
+  },
+  {
+    "revision": "4390848130c5c2686de728c41877f2d7",
+    "url": "/favicon-48x48.png"
+  },
+  {
+    "revision": "c9340357e428b3f608f8a3cb3b2bcb33",
+    "url": "/gengduo@2x.png"
+  },
+  {
+    "revision": "d0b80418951ea16b49b90b62f9bca76b",
+    "url": "/img/sdw/SDW-blue.png"
+  },
+  {
+    "revision": "16f4300c23d6408ed705fbfa7a10345f",
+    "url": "/img/sdw/SDW-logo@2x.png"
+  },
+  {
+    "revision": "00739e1d8858ece4dc13086858cf7370",
+    "url": "/img/sdw/SDW.png"
+  },
+  {
+    "revision": "8ed4ee1178c2408cc161b943858239f9",
+    "url": "/img/sdw/UN-check box.png"
+  },
+  {
+    "revision": "16118a366e7e07ee6e9f5f9bf12a72e7",
+    "url": "/img/sdw/Uni V2.png"
+  },
+  {
+    "revision": "671c89aeb8d865eda24fff211eeacdef",
+    "url": "/img/sdw/arrow-down-small.png"
+  },
+  {
+    "revision": "d828f7062515b1ffa3b077d54505c980",
+    "url": "/img/sdw/arrow-down@2x.png"
+  },
+  {
+    "revision": "34ba0dd8834c49c4e0ec812fbb1a54aa",
+    "url": "/img/sdw/arrow-right-active.png"
+  },
+  {
+    "revision": "8d2dc5f95a456df6ebbb78342f33ba1c",
+    "url": "/img/sdw/arrow-right.png"
+  },
+  {
+    "revision": "beaf97b7156d39ea92cb5ab189d10ce2",
+    "url": "/img/sdw/arrow-up@2x.png"
+  },
+  {
+    "revision": "475eea51d43a5c48bc89985029c66af7",
+    "url": "/img/sdw/back.png"
+  },
+  {
+    "revision": "53f5a874779c30c8b4d455054a110f0c",
+    "url": "/img/sdw/check box.png"
+  },
+  {
+    "revision": "47d827b7b0d6dc485b017c40b52118fe",
+    "url": "/img/sdw/dai.png"
+  },
+  {
+    "revision": "200b35ca196a43534cd041134e1e2121",
+    "url": "/img/sdw/down.png"
+  },
+  {
+    "revision": "a666e32e3cb664192dc4407345bdc70f",
+    "url": "/img/sdw/lp token.png"
+  },
+  {
+    "revision": "c2fec35a076c246376b27b0ed15d841b",
+    "url": "/img/sdw/susd.png"
+  },
+  {
+    "revision": "1d1909da76d4c6c4894d5ccf5ed6b180",
+    "url": "/img/sdw/sync-solid.png"
+  },
+  {
+    "revision": "8786c4156ce9fc52a47df2527737eeb5",
+    "url": "/img/sdw/usdc.png"
+  },
+  {
+    "revision": "bfd55d31b2f6bde6a55c34ad58b71d1b",
+    "url": "/img/sdw/usdt.png"
+  },
+  {
+    "revision": "f3c3908dd1cb051c594bf9536e989223",
+    "url": "/img/sdw/zhuanhaun-sspng.png"
+  },
+  {
+    "revision": "7ed910ef2d2c7f77309c0ff796d0fd2d",
+    "url": "/img/sdw/zhuanhaun.png"
+  },
+  {
+    "revision": "4bb4bac75c78b75d0b77f7090d2fd18a",
+    "url": "/img/sdw/zhuanhaun_un.png"
+  },
+  {
+    "revision": "f257e551c2767837b8415f108d938868",
+    "url": "/newfavicon.svg"
+  },
+  {
+    "revision": "08b91ec6b29cee1dd9e05cdfa532329d",
+    "url": "/parallel.cash@2x.png"
+  },
+  {
+    "revision": "e518d69aeb0e270df2ba3e7583f0bac2",
+    "url": "/qrcode-solid.svg"
+  },
+  {
+    "revision": "53f5a874779c30c8b4d455054a110f0c",
+    "url": "/radio-checked@2x.png"
+  },
+  {
+    "revision": "8ed4ee1178c2408cc161b943858239f9",
+    "url": "/radio@2x.png"
+  },
+  {
+    "revision": "248b9d625f793717be5b69e4971f8b3e",
+    "url": "/sync-solid.svg"
+  },
+  {
+    "revision": "2ba48c7ad2c727937efc35873c44f6d6",
+    "url": "/tokens/IEARN-icon.png"
+  },
+  {
+    "revision": "a1d8eb953ddb1ce942acfb58df1e9f69",
+    "url": "/tokens/_ybusd.png"
+  },
+  {
+    "revision": "78e3bfd191f532cf904510327ca44e07",
+    "url": "/tokens/_ydai.png"
+  },
+  {
+    "revision": "7a717f82fe9b2660e8a5e6b915281bfd",
+    "url": "/tokens/_ysusd.png"
+  },
+  {
+    "revision": "0f2e130e8d70d5b5495617a83dfcd33d",
+    "url": "/tokens/_ytusd.png"
+  },
+  {
+    "revision": "802b73d65fd8904431f08fcf257ecf28",
+    "url": "/tokens/_yusdc.png"
+  },
+  {
+    "revision": "62be285d781cffe27f927e7f2b6a38e6",
+    "url": "/tokens/_yusdt.png"
+  },
+  {
+    "revision": "3d451ef8ca26c5eb7195c60c0f2e53c3",
+    "url": "/tokens/android-icon-192x192.png"
+  },
+  {
+    "revision": "ec6279c61572c949bfcffc89c8c616df",
+    "url": "/tokens/apple-icon.png"
+  },
+  {
+    "revision": "e157bcf70d04cd68d709b2622d53bbd7",
+    "url": "/tokens/btc.svg"
+  },
+  {
+    "revision": "69fc6580c3558098242c2a1e90906ae3",
+    "url": "/tokens/busd.png"
+  },
+  {
+    "revision": "e5ea333232e7e3e9f4b6a3bddace949b",
+    "url": "/tokens/cdai.svg"
+  },
+  {
+    "revision": "c1e7ba6cd99a094f2c159b562e33bbc8",
+    "url": "/tokens/chi.png"
+  },
+  {
+    "revision": "f146f14fb355100b3fbdd1f22b7ffd46",
+    "url": "/tokens/cusdc.svg"
+  },
+  {
+    "revision": "6a47d1620efdb46d9ee52cd708558c4f",
+    "url": "/tokens/dai.png"
+  },
+  {
+    "revision": "fd2080622924be09c309402a5a6f9b86",
+    "url": "/tokens/dai.svg"
+  },
+  {
+    "revision": "837c0669967301a493074a9d8fa10315",
+    "url": "/tokens/favicon-96x96.png"
+  },
+  {
+    "revision": "ed16db49dd8ef710f7fbb33bbe9a8854",
+    "url": "/tokens/hbtc.png"
+  },
+  {
+    "revision": "2ba48c7ad2c727937efc35873c44f6d6",
+    "url": "/tokens/iearn.png"
+  },
+  {
+    "revision": "c59fcc3126678301fa433b67bf544cc3",
+    "url": "/tokens/pax.png"
+  },
+  {
+    "revision": "c3f431cec5d142744e520374d73c54dd",
+    "url": "/tokens/renbtc.svg"
+  },
+  {
+    "revision": "47289192665b220f022d1c54df6385c8",
+    "url": "/tokens/sbtc.svg"
+  },
+  {
+    "revision": "b962c6fe1e9a4bebd8245afcd693b374",
+    "url": "/tokens/susd.svg"
+  },
+  {
+    "revision": "f7aff8fab6766c32b657145fe625832f",
+    "url": "/tokens/tbtc.png"
+  },
+  {
+    "revision": "2882c8901229a9507959caa311590660",
+    "url": "/tokens/tusd.svg"
+  },
+  {
+    "revision": "ce664acfbdfb5bfa2bec8f3af7e3b08b",
+    "url": "/tokens/usdc.svg"
+  },
+  {
+    "revision": "37b5678f154dc4dd01175d5c8354ec3d",
+    "url": "/tokens/usdt.svg"
+  },
+  {
+    "revision": "49e251726ef14a987f2d6d367ede6b92",
+    "url": "/tokens/usdtlarge.svg"
+  },
+  {
+    "revision": "f524516c2f9dc2566a901f1a9250485a",
+    "url": "/tokens/wbtc.svg"
+  },
+  {
+    "revision": "5b933108d659c75707b766037323df62",
+    "url": "/tokens/ybusd.png"
+  },
+  {
+    "revision": "3173d3d70614d2868c201d09c2aa4c91",
+    "url": "/tokens/ydai.png"
+  },
+  {
+    "revision": "4ddb40601ec8bbc584cd12e108bf6523",
+    "url": "/tokens/ydai2.png"
+  },
+  {
+    "revision": "4def28a37afc184c7ccd26e6a6db400d",
+    "url": "/tokens/ydai3.png"
+  },
+  {
+    "revision": "8d03e4ec0a217ce09f09e2fe6c2b05b9",
+    "url": "/tokens/ydai4.png"
+  },
+  {
+    "revision": "f824cedc7b08e0e02f6a43e0a4fc0087",
+    "url": "/tokens/ysusd.png"
+  },
+  {
+    "revision": "3d19cddfc28bd2d6529f903cd09d52b7",
+    "url": "/tokens/ytusd.png"
+  },
+  {
+    "revision": "bdb6ff351c908e4fd36b943a73f86f90",
+    "url": "/tokens/yusdc.png"
+  },
+  {
+    "revision": "33f812a9bd4af1e63f7f0bef8f54d950",
+    "url": "/tokens/yusdc2.png"
+  },
+  {
+    "revision": "d9791e92432144f062dfb58c9e0ecd40",
+    "url": "/tokens/yusdt.png"
+  },
+  {
+    "revision": "3e0e37319a068fee5b6e0b644e39f120",
+    "url": "/tokens/yusdt3.png"
+  },
+  {
+    "revision": "ce8e2a4156448bf45ac70338ba0093f0",
+    "url": "/trash-alt-solid.svg"
+  },
+  {
+    "revision": "aace94421f7bf5827991367a804d2781",
+    "url": "/zhuanhaun@2x.png"
+  }
+]);
